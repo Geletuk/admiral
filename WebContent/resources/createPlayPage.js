bc195fd0f0daf8987ede78c45abacde114f43b7b
ri-lopatina <ri-lopatina@yandex.ru>
2017-09-30 10:41:02 +0300

    refactoring functions

--- WebContent/resources/createPlayPage.js
+++ WebContent/resources/createPlayPage.js
@@ -43,48 +43,58 @@ function createTable()
 function createChipContainer() {
 	var chip_img_id = '';
 	var chip_div_id = '';
-	var elem = document.getElementById("chip-container");
-	for (i = 0; i < chip_pictures.length; i++) 
+	var div_chips_container = document.getElementById("chip-container");
+	for (i = 0; i < chip_amount.length; i++) 
 	{
-		chip_div_id = "chip_div_" + chip_names[i];
-		if (chip_pictures[i] == "resources/images/Torpeda.jpg")
+		if(chip_amount[i] != 0)
 		{
-			chip_img_id = "pihc_img_" + chip_names[i];
+			createShipRow(chip_names[i], chip_amount[i], div_chips_container);
 		}
-		else if (chip_pictures[i] == "resources/images/Aircraft.jpg")
-		{
-			chip_img_id = "pihc_img_" + chip_names[i];
-		} else {
-			chip_img_id = "chip_img_" + chip_names[i];
-		}
-
-		var	div_row = elem.appendChild(document.createElement('div'));
-		div_row.setAttribute( "class", "row" );		
-		var div_col4 = div_row.appendChild(document.createElement('div'));
-		div_col4.setAttribute("class", "col-md-4");
-		div_col4.setAttribute("id", chip_div_id);  
-			
-		for (j = 0; j < chip_amount[i]; j++)
-		{
-			var div_elem_img = div_col4.appendChild(document.createElement('img'));		
-			div_elem_img.setAttribute("id", chip_img_id + j);
-			div_elem_img.setAttribute("src", chip_pictures[i]);	
-			div_elem_img.setAttribute("height", "20");
-			div_elem_img.setAttribute("width", "40");
-			div_elem_img.style.zIndex = j;
-		}		
-		var div_col3 = div_row.appendChild(document.createElement('div'));
-		div_col3.setAttribute("class", "col-md-3");
-		
-		var elem_amount = div_col3.appendChild(document.createElement('p'));
-		elem_amount.innerHTML = chip_amount[i];
-		elem_amount.setAttribute( "class", "p_text" );
-		
-		var div_col5 = div_row.appendChild(document.createElement('div'));
-		div_col5.setAttribute("class", "col-md-5");
-		
-		var elem_name = div_col5.appendChild(document.createElement('p'));
-		elem_name.innerHTML = chip_names[i];	
 	}	
 	$('img[id*=img_Torpeda], img[id*=img_Aircraft]').addClass('disabled').fadeTo(0, 0.5);
+}
+
+
+function createShipRow(shipName, amount, div_chips_container)
+{
+	chip_div_id = "chip_div_" + shipName;
+	var	div_row = div_chips_container.appendChild(document.createElement('div'));
+	div_row.setAttribute( "class", "row" );		
+	var div_col4 = div_row.appendChild(document.createElement('div'));
+	div_col4.setAttribute("class", "col-md-4");
+	div_col4.setAttribute("id", chip_div_id);  
+		
+	for (ship_num = 0; j < amount; j++)
+	{
+		createShip(shipName, ship_num, div_col4);
+
+	}		
+	var div_col3 = div_row.appendChild(document.createElement('div'));
+	div_col3.setAttribute("class", "col-md-3");
+	
+	var elem_amount = div_col3.appendChild(document.createElement('p'));
+	elem_amount.innerHTML = amount;
+	elem_amount.setAttribute( "class", "p_text" );
+	
+	var div_col5 = div_row.appendChild(document.createElement('div'));
+	div_col5.setAttribute("class", "col-md-5");
+	
+	var elem_name = div_col5.appendChild(document.createElement('p'));
+	elem_name.innerHTML = shipName;	
+}
+
+function createShip(shipName, ship_num, div_contaner)
+{
+	if (shipName == "Torpeda" || shipName == "Aircraft")
+	{
+		chip_img_id = "pihc_img_" + shipName;
+	} else {
+		chip_img_id = "chip_img_" + shipName;
+	}
+	var div_elem_img = div_contaner.appendChild(document.createElement('img'));		
+	div_elem_img.setAttribute("id", chip_img_id + ship_num);
+	div_elem_img.setAttribute("src", "resources/images/" + shipName + ".jpg");	
+	div_elem_img.setAttribute("height", "20");
+	div_elem_img.setAttribute("width", "40");
+	div_elem_img.style.zIndex = ship_num;
 }
